dbd110cc311d694f4c52414a16c0cbb9829ac083
ryu467767 <ryushi.kokoro.dimension.0812@gmail.com>
2026-04-20 23:23:43 +0900

    add: SNSアカウント一括更新SQL

---
+++ sns_update.sql
@@ -0,0 +1,153 @@
+-- =====================================================
+-- 水族館 SNSアカウント 一括更新SQL
+-- 調査日: 2026-04-20
+-- 本番適用前に必ずご確認ください
+-- =====================================================
+
+-- 北海道
+UPDATE aquariums SET twitter_id='Rynca_chan', instagram_id='sapporo_salmon_museum' WHERE id=1;
+UPDATE aquariums SET twitter_id='sunpi_aquarium', instagram_id='sunpiazza_aquarium' WHERE id=2;
+UPDATE aquariums SET twitter_id='aoao_sapporo', instagram_id='aoao_sapporo' WHERE id=3;
+UPDATE aquariums SET twitter_id='chito_se_ame', instagram_id='chitose_aq' WHERE id=4;
+UPDATE aquariums SET twitter_id='otaruaquarium', instagram_id='otaru_aquarium' WHERE id=5;
+UPDATE aquariums SET twitter_id='marinepark_nixe', instagram_id='noboribetsumarineparknixe' WHERE id=6;
+UPDATE aquariums SET twitter_id='muroranaquarium', instagram_id='aburabouzusan' WHERE id=7;
+UPDATE aquariums SET twitter_id='shibetsu_salmon', instagram_id='shibetsu_salmon_museum' WHERE id=9;
+UPDATE aquariums SET twitter_id='onneyu_aqua', instagram_id='onneyuaqua_official' WHERE id=10;
+UPDATE aquariums SET twitter_id='kusiro_pukupuku', instagram_id='kushiro.aquarium.pukupuku' WHERE id=11;
+UPDATE aquariums SET instagram_id='wakkariumu' WHERE id=14;
+UPDATE aquariums SET twitter_id='asahiyamazoo1', instagram_id='asahiyamazoo1' WHERE id=15;
+
+-- 東北
+UPDATE aquariums SET twitter_id='AsamushiAqua', instagram_id='asamushi_aquarium' WHERE id=17;
+UPDATE aquariums SET instagram_id='marient_7800' WHERE id=18;
+UPDATE aquariums SET twitter_id='moguranpiaqua', instagram_id='moguranpia_kamekiti' WHERE id=19;
+UPDATE aquariums SET twitter_id='ogaaquariumgao', instagram_id='oga_aquarium_gao' WHERE id=21;
+UPDATE aquariums SET twitter_id='AQUATERRACE_N', instagram_id='aquaterrace_nishikigaoka' WHERE id=22;
+UPDATE aquariums SET twitter_id='sendaiuminomori', instagram_id='uminomori_official' WHERE id=23;
+UPDATE aquariums SET twitter_id='sendaisiyagiya' WHERE id=24;
+UPDATE aquariums SET twitter_id='JELLYFISHAQ', instagram_id='kamoaquarium' WHERE id=26;
+UPDATE aquariums SET twitter_id='aquamarinestaff', instagram_id='aquamarinestaff' WHERE id=27;
+UPDATE aquariums SET twitter_id='InawashiroAQ', instagram_id='kawasemiaquarium' WHERE id=28;
+
+-- 関東
+UPDATE aquariums SET twitter_id='tnapofficial', instagram_id='nakagawa_aquatic_park' WHERE id=29;
+UPDATE aquariums SET instagram_id='kiryuzoo' WHERE id=31;
+UPDATE aquariums SET twitter_id='aw_oarai', instagram_id='aquaworld_oarai' WHERE id=32;
+UPDATE aquariums SET twitter_id='saitamaaqua', instagram_id='saitama_aquarium' WHERE id=34;
+UPDATE aquariums SET twitter_id='kamoseaOfficial', instagram_id='kamogawaseaworld' WHERE id=36;
+UPDATE aquariums SET instagram_id='itabashi_nettaikan' WHERE id=38;
+UPDATE aquariums SET twitter_id='seibutuen_info', instagram_id='seibutuen' WHERE id=39;
+UPDATE aquariums SET twitter_id='Shinagawa_Aqua', instagram_id='shinasui_insta' WHERE id=40;
+UPDATE aquariums SET twitter_id='_aquapark_', instagram_id='aquapark_official' WHERE id=41;
+UPDATE aquariums SET instagram_id='gakyo_uwsaquarium' WHERE id=42;
+UPDATE aquariums SET twitter_id='Sumida_Aquarium', instagram_id='sumida_aquarium_' WHERE id=43;
+UPDATE aquariums SET twitter_id='KasaiSuizokuen' WHERE id=44;
+UPDATE aquariums SET twitter_id='Sunshine_Aqua', instagram_id='sunshine_aquarium' WHERE id=45;
+UPDATE aquariums SET twitter_id='_seaparadise_', instagram_id='seaparadise_official' WHERE id=50;
+UPDATE aquariums SET twitter_id='fortuneaquarium', instagram_id='fortune_aquarium' WHERE id=51;
+UPDATE aquariums SET twitter_id='enosui_com', instagram_id='enosui_com' WHERE id=52;
+UPDATE aquariums SET twitter_id='aq_sagamigawa', instagram_id='sagamihara_aquarium' WHERE id=54;
+UPDATE aquariums SET twitter_id='aqualabostaff', instagram_id='kitasatomb' WHERE id=55;
+UPDATE aquariums SET twitter_id='kawasui_aqua', instagram_id='kawasui_aqua' WHERE id=57;
+
+-- 甲信越・北陸
+UPDATE aquariums SET twitter_id='Marinepia_PR', instagram_id='marinepia_nihonkai_aquarium' WHERE id=59;
+UPDATE aquariums SET twitter_id='tera_sui' WHERE id=60;
+UPDATE aquariums SET twitter_id='_umigatari_', instagram_id='umigatari_official' WHERE id=61;
+UPDATE aquariums SET instagram_id='uozuaquarium_official' WHERE id=64;
+UPDATE aquariums SET twitter_id='Itasenpara1927' WHERE id=65;
+UPDATE aquariums SET twitter_id='notoaqua_jp', instagram_id='notojima_aquarium' WHERE id=66;
+UPDATE aquariums SET instagram_id='kunimikurage' WHERE id=68;
+UPDATE aquariums SET twitter_id='echiaqua', instagram_id='echiaqua' WHERE id=69;
+UPDATE aquariums SET instagram_id='kaihin_marin' WHERE id=71;
+
+-- 東海
+UPDATE aquariums SET twitter_id='aquatotto', instagram_id='aquatotto_gifu' WHERE id=72;
+UPDATE aquariums SET twitter_id='tateshinaaqua' WHERE id=75;
+UPDATE aquariums SET twitter_id='smartaqua_sz', instagram_id='smartaqua_sz' WHERE id=77;
+UPDATE aquariums SET twitter_id='NumazuDeepsea', instagram_id='shinkaisuizokukan_official' WHERE id=78;
+UPDATE aquariums SET twitter_id='izu_mitosea', instagram_id='mitosea_official' WHERE id=79;
+UPDATE aquariums SET twitter_id='aquariumshimoda', instagram_id='shimoda_aquarium' WHERE id=80;
+UPDATE aquariums SET twitter_id='iZoo_iZoo_', instagram_id='izoo_reptiles' WHERE id=81;
+UPDATE aquariums SET twitter_id='bananawanien_82', instagram_id='bananawani_official' WHERE id=82;
+UPDATE aquariums SET twitter_id='uxotto', instagram_id='uxotto' WHERE id=84;
+UPDATE aquariums SET instagram_id='yogyo_aquarium' WHERE id=85;
+UPDATE aquariums SET twitter_id='port_nagoaqu', instagram_id='port_of_nagoya_public_aquarium' WHERE id=87;
+UPDATE aquariums SET twitter_id='higashiyamapark', instagram_id='higashiyamapark' WHERE id=88;
+UPDATE aquariums SET twitter_id='beachland2000', instagram_id='beachland_aquarium' WHERE id=90;
+UPDATE aquariums SET twitter_id='takesuiaquarium', instagram_id='takeshimaaquarium' WHERE id=91;
+UPDATE aquariums SET twitter_id='Hekinan_Aqua', instagram_id='hekinanaqua_official' WHERE id=92;
+UPDATE aquariums SET twitter_id='non_hoi_park', instagram_id='non_hoi_park_animals' WHERE id=93;
+
+-- 近畿
+UPDATE aquariums SET twitter_id='TOBA_AQUARIUM', instagram_id='toba_aquarium.official' WHERE id=96;
+UPDATE aquariums SET twitter_id='iseseaparadise', instagram_id='iseseaparadise' WHERE id=97;
+UPDATE aquariums SET instagram_id='akame_aquarium' WHERE id=98;
+UPDATE aquariums SET instagram_id='biwakobase' WHERE id=99;
+UPDATE aquariums SET twitter_id='biwahaku', instagram_id='biwahaku' WHERE id=100;
+UPDATE aquariums SET twitter_id='Kyoto_Aquarium', instagram_id='kyoto_aquarium' WHERE id=102;
+UPDATE aquariums SET twitter_id='Osaka_Kaiyukan', instagram_id='kaiyukan_japan' WHERE id=104;
+UPDATE aquariums SET twitter_id='NIFREL_official', instagram_id='nifrel_official' WHERE id=106;
+UPDATE aquariums SET twitter_id='kinpaku_museum', instagram_id='kinpaku_museum' WHERE id=107;
+UPDATE aquariums SET twitter_id='Kobe_SumaSea', instagram_id='kobe_sumasea' WHERE id=109;
+UPDATE aquariums SET twitter_id='atoa_kobe', instagram_id='atoa_kobe' WHERE id=110;
+UPDATE aquariums SET twitter_id='minatoyama_jp' WHERE id=111;
+UPDATE aquariums SET instagram_id='himeji_aquarium' WHERE id=112;
+UPDATE aquariums SET instagram_id='kinosakimarineworld' WHERE id=113;
+UPDATE aquariums SET instagram_id='janohire_official' WHERE id=115;
+UPDATE aquariums SET twitter_id='Shirahama_Aqua' WHERE id=118;
+UPDATE aquariums SET twitter_id='aws_official', instagram_id='adventureworld_official' WHERE id=119;
+UPDATE aquariums SET twitter_id='KushimotoMP', instagram_id='kushimoto_marine_park_official' WHERE id=120;
+UPDATE aquariums SET twitter_id='ebikaniaquarium', instagram_id='ebikaniaquarium' WHERE id=121;
+UPDATE aquariums SET instagram_id='taiji_whale_museum' WHERE id=122;
+UPDATE aquariums SET instagram_id='dolphinresort.ig' WHERE id=123;
+UPDATE aquariums SET instagram_id='dolphinbase' WHERE id=124;
+
+-- 中国
+UPDATE aquariums SET instagram_id='kanikkokan_official' WHERE id=125;
+UPDATE aquariums SET twitter_id='AQUASSHIMANE', instagram_id='aquas.shimane' WHERE id=126;
+UPDATE aquariums SET instagram_id='gobius_c' WHERE id=127;
+UPDATE aquariums SET twitter_id='horseshoecrab_m' WHERE id=128;
+UPDATE aquariums SET twitter_id='shibukawamarine', instagram_id='shibumari_official' WHERE id=129;
+UPDATE aquariums SET twitter_id='aquarium_hrsm', instagram_id='aquarium_hiroshima' WHERE id=130;
+UPDATE aquariums SET twitter_id='miyajima_aqua', instagram_id='miyajima_aqua' WHERE id=132;
+UPDATE aquariums SET instagram_id='fukuyama_u_marinebiocenter' WHERE id=133;
+UPDATE aquariums SET twitter_id='shimonoseki_aq', instagram_id='shimonoseki_aquarium_kaikyokan' WHERE id=134;
+
+-- 四国
+UPDATE aquariums SET twitter_id='newyashimaaq', instagram_id='newyashimaaquarium' WHERE id=137;
+UPDATE aquariums SET twitter_id='shikokuaquarium', instagram_id='shikoku_aquarium' WHERE id=138;
+UPDATE aquariums SET twitter_id='dolphincenter' WHERE id=139;
+UPDATE aquariums SET twitter_id='caretta3' WHERE id=140;
+UPDATE aquariums SET instagram_id='marinejam_kaiyo' WHERE id=141;
+UPDATE aquariums SET instagram_id='osakanakan.nijinomori' WHERE id=142;
+UPDATE aquariums SET instagram_id='dolphinfarm.shimanami' WHERE id=143;
+UPDATE aquariums SET twitter_id='katurahama_aq', instagram_id='katurahama_aqua' WHERE id=145;
+UPDATE aquariums SET twitter_id='kaiyoukan', instagram_id='kaiyoukan' WHERE id=146;
+UPDATE aquariums SET twitter_id='murosui_kochi' WHERE id=148;
+UPDATE aquariums SET twitter_id='Mdolphincenter', instagram_id='murotodc' WHERE id=149;
+
+-- 九州
+UPDATE aquariums SET twitter_id='marine_uminaka', instagram_id='marineworld.uminaka' WHERE id=150;
+UPDATE aquariums SET twitter_id='mizukan_8crew' WHERE id=151;
+UPDATE aquariums SET twitter_id='ariake_aquarium' WHERE id=152;
+UPDATE aquariums SET twitter_id='HigataKouryuKan' WHERE id=153;
+UPDATE aquariums SET twitter_id='umitamago_staff', instagram_id='umitamago_staff' WHERE id=155;
+UPDATE aquariums SET twitter_id='irukajima_staff', instagram_id='irukajima.staff' WHERE id=158;
+UPDATE aquariums SET twitter_id='ooyodo_study' WHERE id=159;
+UPDATE aquariums SET twitter_id='iruka_land' WHERE id=163;
+UPDATE aquariums SET twitter_id='NagasakiPengin', instagram_id='nagasakipenguin' WHERE id=165;
+UPDATE aquariums SET twitter_id='kujukushima', instagram_id='kujukushima_pearl_sea_resort' WHERE id=166;
+UPDATE aquariums SET twitter_id='iki_irukapark', instagram_id='ikiirukapark' WHERE id=168;
+UPDATE aquariums SET twitter_id='ioworld_aqua', instagram_id='kagoshima_city_aquarium' WHERE id=169;
+UPDATE aquariums SET twitter_id='aktkariri' WHERE id=170;
+
+-- 沖縄
+UPDATE aquariums SET twitter_id='kaiyohakukoen', instagram_id='kaiyohaku_churaumi' WHERE id=171;
+UPDATE aquariums SET twitter_id='dmm_kariyushi', instagram_id='dmm_aquarium' WHERE id=172;
+UPDATE aquariums SET instagram_id='seaseed.ltd' WHERE id=173;
+
+-- その他
+UPDATE aquariums SET instagram_id='nobeokamarine' WHERE id=175;
+UPDATE aquariums SET twitter_id='ageshimaumicafe', instagram_id='ageshima' WHERE id=176;
