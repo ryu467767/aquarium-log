3b608431c49266a1f50379e2b10c1408cf1e4dd2
ryu467767 <ryushi.kokoro.dimension.0812@gmail.com>
2026-04-21 23:15:43 +0900

    fix: add_awashima.pyにlocation_rawを追加

--- add_awashima.py
+++ add_awashima.py
@@ -1,8 +1,8 @@
 import sqlite3
 c = sqlite3.connect('/data/app.db')
 c.execute("""
-INSERT INTO aquariums (name,prefecture,city,url,mola_star,lat,lng,has_dolphin,has_sealion,twitter_id,instagram_id,is_closed)
-VALUES ('あわしまマリンパーク','静岡県','沼津市','https://www.marinepark.jp/',0,35.033468,138.890185,1,1,'A_shimatarou','awashima_marinepark',0)
+INSERT INTO aquariums (name,prefecture,city,location_raw,url,mola_star,lat,lng,has_dolphin,has_sealion,twitter_id,instagram_id,is_closed)
+VALUES ('あわしまマリンパーク','静岡県','沼津市','静岡県沼津市内浦重寺186','https://www.marinepark.jp/',0,35.033468,138.890185,1,1,'A_shimatarou','awashima_marinepark',0)
 """)
 c.commit()
 print(c.total_changes, 'rows inserted')
